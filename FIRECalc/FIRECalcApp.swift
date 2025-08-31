7cdc7723ed224a0885088a792b575f284eb5944e
Drew Sullivan <drew.sullivan.dma@gmail.com>
2025-08-31 14:37:32 -0600

    Initial Commit

---
+++ FIRECalc/FIRECalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIRECalcApp.swift
+//  FIRECalc
+//
+//  Created by Drew Sullivan on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct FIRECalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
